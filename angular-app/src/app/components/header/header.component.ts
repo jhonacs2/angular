6a11c0336c607b6c8862af263f8031d66458de0f
Esther White <monacodelisa@gmail.com>
2024-03-03 15:28:29 +0200

    simplifying with ternary in header logo image

--- angular-app/src/app/components/header/header.component.ts
+++ angular-app/src/app/components/header/header.component.ts
@@ -1,71 +1,66 @@
-import { Component, OnInit, OnDestroy, inject } from '@angular/core';
-import { ThemeService } from '../../services/theme.service';
-import { Subscription } from 'rxjs';
-import { BlogService } from '../../services/blog.service';
+import { Component, OnInit, OnDestroy, inject } from "@angular/core";
+import { ThemeService } from "../../services/theme.service";
+import { Subscription } from "rxjs";
+import { BlogService } from "../../services/blog.service";
 
-import { KeyValuePipe } from '@angular/common';
-import { BlogInfo, BlogLinks, } from '../../models/blog-info';
-import { RouterLink } from '@angular/router';
-import { SeriesList } from '../../models/post';
-import { FollowDialogComponent } from '../../partials/follow-dialog/follow-dialog.component';
-import { ModalService } from '../../services/modal.service';
+import { KeyValuePipe } from "@angular/common";
+import { BlogInfo, BlogLinks } from "../../models/blog-info";
+import { RouterLink } from "@angular/router";
+import { SeriesList } from "../../models/post";
+import { FollowDialogComponent } from "../../partials/follow-dialog/follow-dialog.component";
+import { ModalService } from "../../services/modal.service";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [KeyValuePipe, RouterLink, FollowDialogComponent],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+	selector: "app-header",
+	standalone: true,
+	imports: [KeyValuePipe, RouterLink, FollowDialogComponent],
+	templateUrl: "./header.component.html",
+	styleUrl: "./header.component.scss",
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  blogInfo!: BlogInfo;
-  blogName: string = '';
-  // start with default image to prevent 404 when returning from post-details page
-  blogImage: string = '/assets/images/AnguHashBlog-logo.jpg';
-  blogSocialLinks!: BlogLinks;
-  seriesList!: SeriesList[];
-  themeService: ThemeService = inject(ThemeService);
-  blogService: BlogService = inject(BlogService);
-  modalService: ModalService = inject(ModalService);
-  private querySubscription?: Subscription;
+	blogInfo!: BlogInfo;
+	blogName: string = "";
+	// start with default image to prevent 404 when returning from post-details page
+	blogImage: string = "/assets/images/anguhashblog-logo.jpg";
+	blogSocialLinks!: BlogLinks;
+	seriesList!: SeriesList[];
+	themeService: ThemeService = inject(ThemeService);
+	blogService: BlogService = inject(BlogService);
+	modalService: ModalService = inject(ModalService);
+	private querySubscription?: Subscription;
 
-  ngOnInit(): void {
-    this.querySubscription = this.blogService
-      .getBlogInfo()
-      .subscribe((data) => {
-        this.blogInfo = data;
-        this.blogName = this.blogInfo.title;
-        if (this.blogInfo.isTeam && this.blogInfo.favicon) {
-          this.blogImage = this.blogInfo.favicon;
-        } else {
-          this.blogImage = '/assets/images/AnguHashBlog-logo.jpg'
-        }
-        if (!this.blogInfo.isTeam) {
-          this.blogService
-          .getAuthorInfo()
-          .subscribe((data) => {
-            if (data.profilePicture) {
-              this.blogImage = data.profilePicture;
-            } else {
-              this.blogImage = '/assets/images/AnguHashBlog-logo.jpg'
-            }
-          });
-        }
-        const { __typename, ...links } = data.links;
-        this.blogSocialLinks = links;
-      });
-    this.querySubscription = this.blogService
-      .getSeriesList()
-      .subscribe((data) => {
-        this.seriesList = data;
-      });
-  }
+	ngOnInit(): void {
+		this.querySubscription = this.blogService
+			.getBlogInfo()
+			.subscribe((data) => {
+				this.blogInfo = data;
+				this.blogName = this.blogInfo.title;
+				this.blogImage =
+					this.blogInfo.isTeam && this.blogInfo.favicon
+						? (this.blogImage = this.blogInfo.favicon)
+						: "/assets/images/anguhashblog-logo.jpg";
+				if (!this.blogInfo.isTeam) {
+					this.blogService.getAuthorInfo().subscribe((data) => {
+						this.blogImage = data.profilePicture
+							? data.profilePicture
+							: "/assets/images/anguhashblog-logo.jpg";
+					});
+				}
+				const { __typename, ...links } = data.links;
+				this.blogSocialLinks = links;
+			});
+		this.querySubscription = this.blogService
+			.getSeriesList()
+			.subscribe((data) => {
+				this.seriesList = data;
+			});
+	}
 
-  toggleTheme(): void {
-    this.themeService.updateTheme();
-  }
+	toggleTheme(): void {
+		this.themeService.updateTheme();
+	}
 
-  ngOnDestroy(): void {
-    this.querySubscription?.unsubscribe();
-  }
+	ngOnDestroy(): void {
+		this.querySubscription?.unsubscribe();
+	}
 }
