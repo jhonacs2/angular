63e4d18bb074e5553239234be0dc21d50a4ae2ab
Esther White <monacodelisa@gmail.com>
2024-03-09 21:03:44 +0200

    fix(pipe): using the builtin DomSanitizer pipe

--- angular-app/src/app/components/post-details/post-details.component.ts
+++ angular-app/src/app/components/post-details/post-details.component.ts
@@ -5,60 +5,64 @@ import { Post } from '../../models/post';
 import { Observable, Subscription } from 'rxjs';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ClipboardCopyButtonDirective } from '../../directives/clipboard-copy-button.directive';
-import { SanitizerHtmlPipe } from '../../pipes/sanitizer-html.pipe';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ThemeService } from '../../services/theme.service';
 import { FooterComponent } from '../footer/footer.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 import { BlogInfo } from '../../models/blog-info';
 
 @Component({
-  selector: 'app-post-details',
-  standalone: true,
-  imports: [
-    SidenavComponent,
-    FooterComponent,
-    RouterLink,
-    AsyncPipe,
-    DatePipe,
-    SanitizerHtmlPipe,
-    ClipboardCopyButtonDirective,
-  ],
-  templateUrl: './post-details.component.html',
-  styleUrl: './post-details.component.scss',
+	selector: "app-post-details",
+	standalone: true,
+	imports: [
+		SidenavComponent,
+		FooterComponent,
+		RouterLink,
+		AsyncPipe,
+		DatePipe,
+		ClipboardCopyButtonDirective,
+	],
+	templateUrl: "./post-details.component.html",
+	styleUrl: "./post-details.component.scss",
 })
 export class PostDetailsComponent implements OnInit, OnDestroy {
-  sidenavOpen: boolean = false;
-  blogURL!: string;
-  blogInfo!: BlogInfo;
-  blogName: string = '';
-  post$!: Observable<Post>;
-  themeService: ThemeService = inject(ThemeService);
-  route: ActivatedRoute = inject(ActivatedRoute);
-  private blogService = inject(BlogService);
-  private querySubscription?: Subscription;
+	sidenavOpen: boolean = false;
+	blogURL!: string;
+	blogInfo!: BlogInfo;
+	blogName: string = "";
+	post$!: Observable<Post>;
+	themeService: ThemeService = inject(ThemeService);
+	route: ActivatedRoute = inject(ActivatedRoute);
+  private sanitizer: DomSanitizer = inject(DomSanitizer);
+	private blogService = inject(BlogService);
+	private querySubscription?: Subscription;
 
-  @Input({ required: true }) slug!: string;
+	@Input({ required: true }) slug!: string;
 
-  ngOnInit(): void {
-    this.blogURL = this.blogService.getBlogURL();
-    this.querySubscription = this.blogService
-    .getBlogInfo(this.blogURL)
-    .subscribe((data) => {
-      this.blogInfo = data;
-      this.blogName = this.blogInfo.title;
-    });
-    this.post$ = this.blogService.getSinglePost(this.blogURL, this.slug);
+	ngOnInit(): void {
+		this.blogURL = this.blogService.getBlogURL();
+		this.querySubscription = this.blogService
+			.getBlogInfo(this.blogURL)
+			.subscribe((data) => {
+				this.blogInfo = data;
+				this.blogName = this.blogInfo.title;
+			});
+		this.post$ = this.blogService.getSinglePost(this.blogURL, this.slug);
+	}
+
+	toggleTheme(): void {
+		this.themeService.updateTheme();
+	}
+
+	toggleSidenav() {
+		this.sidenavOpen = !this.sidenavOpen;
+	}
+
+  sanitizeHtml(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
-  toggleTheme(): void {
-    this.themeService.updateTheme();
-  }
-
-  toggleSidenav() {
-    this.sidenavOpen = !this.sidenavOpen;
-  }
-
-  ngOnDestroy(): void {
-    this.querySubscription?.unsubscribe();
-  }
+	ngOnDestroy(): void {
+		this.querySubscription?.unsubscribe();
+	}
 }
