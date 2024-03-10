050f3d5f80ca1ed569a99cb758cd2e70d1c2aab0
Esther White <monacodelisa@gmail.com>
2024-03-10 10:08:21 +0200

    sanitizer as a pipe not directly so directives work, yt video  directive added

--- angular-app/src/app/components/post-details/post-details.component.ts
+++ angular-app/src/app/components/post-details/post-details.component.ts
@@ -1,15 +1,16 @@
-import { AfterViewInit, Component, ElementRef, Renderer2, Input, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
 import { BlogService } from '../../services/blog.service';
 import { AsyncPipe, DatePipe } from '@angular/common';
+import { SanitizerHtmlPipe } from '../../pipes/sanitizer-html.pipe';
 import { Post } from '../../models/post';
 import { Observable, Subscription } from 'rxjs';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ClipboardCopyButtonDirective } from '../../directives/clipboard-copy-button.directive';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ThemeService } from '../../services/theme.service';
 import { FooterComponent } from '../footer/footer.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
 import { BlogInfo } from '../../models/blog-info';
+import { YoutubeVideoDirective } from '../../directives/youtube-video.directive';
 
 @Component({
 	selector: "app-post-details",
@@ -20,12 +21,14 @@ import { BlogInfo } from '../../models/blog-info';
 		RouterLink,
 		AsyncPipe,
 		DatePipe,
+    SanitizerHtmlPipe,
 		ClipboardCopyButtonDirective,
+    YoutubeVideoDirective
 	],
 	templateUrl: "./post-details.component.html",
 	styleUrl: "./post-details.component.scss",
 })
-export class PostDetailsComponent implements OnInit, OnDestroy, AfterViewInit {
+export class PostDetailsComponent implements OnInit, OnDestroy {
 	sidenavOpen: boolean = false;
 	blogURL!: string;
 	blogInfo!: BlogInfo;
@@ -34,12 +37,8 @@ export class PostDetailsComponent implements OnInit, OnDestroy, AfterViewInit {
   modifiedPost!: Post;
 	themeService: ThemeService = inject(ThemeService);
 	route: ActivatedRoute = inject(ActivatedRoute);
-  private sanitizer: DomSanitizer = inject(DomSanitizer);
 	private blogService = inject(BlogService);
 	private querySubscription?: Subscription;
-
-  // private el = inject(ElementRef);
-  private renderer = inject(Renderer2);
 
 	@Input({ required: true }) slug!: string;
 
@@ -61,61 +60,6 @@ export class PostDetailsComponent implements OnInit, OnDestroy, AfterViewInit {
 	toggleSidenav() {
 		this.sidenavOpen = !this.sidenavOpen;
 	}
-
-  ngAfterViewInit() {
-    this.post$.subscribe((post) => {
-      // Log the post data to the console
-      console.log('Post data:', post.content);
-
-      // Extract the HTML content from post
-      let htmlContent = post.content.html;
-
-      // Create a temporary div to parse the HTML
-      let tempDiv = document.createElement('div');
-      tempDiv.innerHTML = htmlContent;
-
-      // Try to access the element here using querySelector
-      let embedWrapperDiv = tempDiv.querySelector('.embed-wrapper');
-			let anchorElement = tempDiv.querySelector('.embed-card');
-			let youtubeUrl = anchorElement?.getAttribute('href');
-			let videoId;
-
-			if (youtubeUrl) {
-				if (youtubeUrl.includes('youtube.com/watch?v=')) {
-					videoId = youtubeUrl.split('v=')[1];
-				} else if (youtubeUrl.includes('youtu.be/')) {
-					videoId = youtubeUrl.split('youtu.be/')[1];
-				}
-			}
-
-			let width = 100; // Set your desired width
-			let height = width * 4.5;
-
-			console.log('Video ID:', videoId);
-
-      if (embedWrapperDiv) {
-        console.log('Element found:', embedWrapperDiv);
-
-        // Set the inner HTML using Renderer
-        this.renderer.setProperty(embedWrapperDiv, 'innerHTML', `<iframe width="100%" height="${height}" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
-
-        // Clone the post object to make modifications
-        this.modifiedPost = { ...post };
-
-        // Assign the modified HTML content to the cloned post object
-        this.modifiedPost.content = { ...post.content, html: tempDiv.innerHTML.toString() };
-
-        // Now you can use the modifiedPost object as needed
-      } else {
-        console.error('Element not found');
-      }
-    });
-  }
-
-
-  sanitizeHtml(html: string): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(html);
-  }
 
 	ngOnDestroy(): void {
 		this.querySubscription?.unsubscribe();
---
+++ angular-app/src/app/directives/youtube-video.directive.spec.ts
@@ -0,0 +1,8 @@
+import { YoutubeVideoDirective } from './youtube-video.directive';
+
+describe('YoutubeVideoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new YoutubeVideoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
