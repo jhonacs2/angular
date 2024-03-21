25268e73fb67c2368e0dd5647cb7b77e799c9e89
Esther White <monacodelisa@gmail.com>
2024-03-21 18:15:50 +0200

    feat(sitewide): initial responsivness mobile media query

--- angular-app/src/app/components/posts/posts.component.ts
+++ angular-app/src/app/components/posts/posts.component.ts
@@ -1,46 +1,52 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { BlogService } from '../../services/blog.service';
-import { RouterLink } from '@angular/router';
-import { AsyncPipe, SlicePipe } from '@angular/common';
-import { PageInfo, Post } from '../../models/post';
-import { InfiniteScrollDirective } from '../../directives/infinite-scroll.directive';
+import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { BlogService } from "../../services/blog.service";
+import { RouterLink } from "@angular/router";
+import { AsyncPipe, SlicePipe } from "@angular/common";
+import { PageInfo, Post } from "../../models/post";
+import { InfiniteScrollDirective } from "../../directives/infinite-scroll.directive";
 
 @Component({
-  selector: 'app-posts',
-  standalone: true,
-  imports: [RouterLink, AsyncPipe, InfiniteScrollDirective, SlicePipe],
-  templateUrl: './posts.component.html',
-  styleUrl: './posts.component.scss',
+	selector: "app-posts",
+	standalone: true,
+	imports: [RouterLink, AsyncPipe, InfiniteScrollDirective, SlicePipe],
+	templateUrl: "./posts.component.html",
+	styleUrl: "./posts.component.scss",
 })
-export class PostsComponent implements OnInit {
-  blogURL!: string;
-  posts!: Post[];
-  blogService: BlogService = inject(BlogService);
-  paginationInfo: PageInfo = { hasNextPage: true, endCursor: '' };
-  isHiddenLoadMore: boolean = true;
-  isActiveInfiniteScroll: boolean = false;
+export class PostsComponent implements OnInit, OnDestroy {
+	blogURL!: string;
+	posts!: Post[];
+	paginationInfo: PageInfo = { hasNextPage: true, endCursor: "" };
+	isHiddenLoadMore: boolean = true;
+	blogService: BlogService = inject(BlogService);
+	isActiveInfiniteScroll: boolean = false;
 
-  ngOnInit() {
-    this.blogURL = this.blogService.getBlogURL();
-    this.loadPosts();
-  }
+	ngOnInit() {
+		this.blogURL = this.blogService.getBlogURL();
+		this.loadPosts();
+	}
 
-  private loadPosts(): void {
-    this.blogService.getPosts(this.blogURL, this.paginationInfo.endCursor).subscribe(postsPageInfo => {
-      this.paginationInfo = postsPageInfo.pagination;
-      this.isHiddenLoadMore = !postsPageInfo.pagination.hasNextPage;
-      this.posts = postsPageInfo.posts;
-    });
-  }
+	private loadPosts(): void {
+		this.blogService
+			.getPosts(this.blogURL, this.paginationInfo.endCursor)
+			.subscribe((postsPageInfo) => {
+				this.paginationInfo = postsPageInfo.pagination;
+				this.isHiddenLoadMore = !postsPageInfo.pagination.hasNextPage;
+				this.posts = postsPageInfo.posts;
+			});
+	}
 
-  loadMorePosts(): void {
-    if (!this.paginationInfo.hasNextPage) return;
-    this.isHiddenLoadMore = true;
-    this.blogService.getPosts(this.blogURL, this.paginationInfo.endCursor).pipe(
-    ).subscribe(newPosts => {
-      this.isActiveInfiniteScroll = true;
-      this.paginationInfo = newPosts.pagination;
-      this.posts = this.posts.concat(newPosts.posts);
-    });
-  }
+	loadMorePosts(): void {
+		if (!this.paginationInfo.hasNextPage) return;
+		this.isHiddenLoadMore = true;
+		this.blogService
+			.getPosts(this.blogURL, this.paginationInfo.endCursor)
+			.pipe()
+			.subscribe((newPosts) => {
+				this.isActiveInfiniteScroll = true;
+				this.paginationInfo = newPosts.pagination;
+				this.posts = this.posts.concat(newPosts.posts);
+			});
+	}
+
+	ngOnDestroy(): void {}
 }
