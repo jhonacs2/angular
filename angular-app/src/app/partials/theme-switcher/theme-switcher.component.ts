023fb2e4420dc20e3c04535198a86655c99f8085
Esther White <monacodelisa@gmail.com>
2024-05-02 10:06:58 +0300

    feat(theme-switcher): custom theme switcher with icons

---
+++ angular-app/src/app/partials/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+
+@Component({
+  selector: 'app-theme-switcher',
+  standalone: true,
+  imports: [],
+  templateUrl: './theme-switcher.component.html',
+  styleUrl: './theme-switcher.component.scss'
+})
+export class ThemeSwitcherComponent {
+  themeService: ThemeService = inject(ThemeService);
+
+  toggleTheme() {
+    this.themeService.updateTheme();
+  }
+}
